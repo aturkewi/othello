56d9e26c7e45488058d36b69793c4dc2e13b24d5
Avidor Turkewitz <aviturkewitz@gmail.com>
2020-03-12 14:37:23 -0400

    deploy

---
+++ precache-manifest.816a3f750de2a4604687e56823c3132f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f4e3f7c75e4256689aba0416142630",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d8d5e96caf0f4002e4a",
+    "url": "./static/css/main.1a4b7947.chunk.css"
+  },
+  {
+    "revision": "7a17b955b4231629b489",
+    "url": "./static/js/2.28f15896.chunk.js"
+  },
+  {
+    "revision": "0d8d5e96caf0f4002e4a",
+    "url": "./static/js/main.11081b0e.chunk.js"
+  },
+  {
+    "revision": "35db4f0c163931364736",
+    "url": "./static/js/runtime-main.405bace9.js"
+  }
+]);
