f5f4e32baa22413755f814d58c54bc943d6084a3
Avidor Turkewitz <aviturkewitz@gmail.com>
2020-03-12 09:57:50 -0400

    add some logging

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echo "building"
 npm run build
+
+echo "deleting branch locally if it exists"
 git branch -D gh-pages
+
 git checkout -b gh-pages &&
 rm -rf static/ &&
 mv ./build/* ./ &&
 git add . &&
 git commit -m "deploy" &&
-git push origin gh-pages:gh-pages &&
+git push origin gh-pages:gh-pages -f &&
 git checkout master
